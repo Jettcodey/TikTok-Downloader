8f13da2a6c338fbced1868f364bf5d1b834a0e9d
Jettcodey <163922510+Jettcodey@users.noreply.github.com>
2024-05-05 01:57:31 +0200

    nothin

--- scripts/playwright-ex.ps1
+++ scripts/playwright-ex.ps1
@@ -1,3 +1,2 @@
 # Execute playwright.ps1 in Powershell 7.
 pwsh -File "C:\Program Files\Jettcodey\TikTok Downloader\TikTok Downloader\playwright.ps1" install firefox
-PAUSE
--- scripts/pwsh.bat
+++ scripts/pwsh.bat
@@ -1,2 +1,2 @@
-REM Install Powershell 7 using winget.
+REM Install Powershell 7 using winget
 winget install --id Microsoft.Powershell --source winget
