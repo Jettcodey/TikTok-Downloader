731f5e4d4e0c19a568134c80751f8b5b6118e75b
Jettcodey <163922510+Jettcodey@users.noreply.github.com>
2025-03-15 13:37:41 +0100

    Remove ToastNotification.cs from .gitignore

---
+++ src/ToastNotification.cs
@@ -0,0 +1,35 @@
+﻿/*
+##########################################
+#           TikTok Downloader            #
+#           Made by Jettcodey            #
+#                © 2024                  #
+#           DO NOT REMOVE THIS           #
+##########################################
+*/
+using System;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace TikTok_Downloader
+{
+    public class ToastNotification
+    {
+        public static bool ToastsAllowed { get; private set; } = true;
+        public static void ToastsAllowedCheckBox(bool value)
+        {
+            ToastsAllowed = value;
+        }
+
+        public static void ShowToast(string title, string message, params object[] args)
+        {
+            if (ToastsAllowed)
+            {
+                string formattedMessage = string.Format(message, args);
+
+                new ToastContentBuilder()
+                    .AddText(title)
+                    .AddText(formattedMessage)
+                    .Show();
+            }
+        }
+    }
+}
