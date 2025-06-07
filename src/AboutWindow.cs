b077b28d8d18ebaa574acdbd52fe9f8ac620ffaf
Jettcodey <163922510+Jettcodey@users.noreply.github.com>
2025-06-07 06:39:22 +0200

    Update AboutWindow to include small License Info

--- src/AboutWindow.cs
+++ src/AboutWindow.cs
@@ -29,6 +29,7 @@ namespace TikTok_Downloader
         }
 
         private Label titleLabel;
+        private Label licenseInfo;
         private Label versionLabel;
         private Label descriptionLabel;
         private Button okButton;
@@ -40,6 +41,7 @@ namespace TikTok_Downloader
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutWindow));
             titleLabel = new Label();
+            licenseInfo = new Label();
             versionLabel = new Label();
             descriptionLabel = new Label();
             okButton = new Button();
@@ -54,18 +56,30 @@ namespace TikTok_Downloader
             titleLabel.BackColor = Color.Transparent;
             titleLabel.Font = new Font("Arial", 12F, FontStyle.Bold);
             titleLabel.ForeColor = Color.Aquamarine;
-            titleLabel.Location = new Point(46, 24);
+            titleLabel.Location = new Point(55, 20);
             titleLabel.Name = "titleLabel";
             titleLabel.Size = new Size(157, 19);
             titleLabel.TabIndex = 0;
             titleLabel.Text = "TikTok Downloader";
+            // 
+            // licenseInfo
+            // 
+            licenseInfo.AutoSize = true;
+            licenseInfo.BackColor = Color.Transparent;
+            licenseInfo.ForeColor = Color.Aqua;
+            licenseInfo.Location = new Point(12, 120);
+            licenseInfo.Name = "licenseInfo";
+            licenseInfo.Size = new Size(249, 15);
+            licenseInfo.TabIndex = 4;
+            licenseInfo.TabStop = true;
+            licenseInfo.Text = "This project is licensed under the MIT License.";
             // 
             // versionLabel
             // 
             versionLabel.AutoSize = true;
             versionLabel.BackColor = Color.Transparent;
             versionLabel.ForeColor = Color.Aqua;
-            versionLabel.Location = new Point(65, 65);
+            versionLabel.Location = new Point(78, 45);
             versionLabel.Name = "versionLabel";
             versionLabel.Size = new Size(117, 15);
             versionLabel.TabIndex = 1;
@@ -76,7 +90,7 @@ namespace TikTok_Downloader
             descriptionLabel.AutoSize = true;
             descriptionLabel.BackColor = Color.Transparent;
             descriptionLabel.ForeColor = Color.Aqua;
-            descriptionLabel.Location = new Point(26, 128);
+            descriptionLabel.Location = new Point(38, 155);
             descriptionLabel.Name = "descriptionLabel";
             descriptionLabel.Size = new Size(196, 15);
             descriptionLabel.TabIndex = 2;
@@ -85,7 +99,7 @@ namespace TikTok_Downloader
             // okButton
             // 
             okButton.BackColor = SystemColors.Control;
-            okButton.Location = new Point(86, 175);
+            okButton.Location = new Point(94, 210);
             okButton.Name = "okButton";
             okButton.Size = new Size(75, 23);
             okButton.TabIndex = 3;
@@ -98,7 +112,7 @@ namespace TikTok_Downloader
             linkLabel.AutoSize = true;
             linkLabel.BackColor = Color.Transparent;
             linkLabel.LinkColor = Color.Aquamarine;
-            linkLabel.Location = new Point(25, 145);
+            linkLabel.Location = new Point(36, 175);
             linkLabel.Name = "linkLabel";
             linkLabel.Size = new Size(199, 15);
             linkLabel.TabIndex = 5;
@@ -112,7 +126,7 @@ namespace TikTok_Downloader
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.ForeColor = Color.Aqua;
-            label2.Location = new Point(71, 80);
+            label2.Location = new Point(78, 80);
             label2.Name = "label2";
             label2.Size = new Size(109, 15);
             label2.TabIndex = 7;
@@ -123,7 +137,7 @@ namespace TikTok_Downloader
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
             label3.ForeColor = Color.Aqua;
-            label3.Location = new Point(8, 95);
+            label3.Location = new Point(14, 100);
             label3.Name = "label3";
             label3.Size = new Size(234, 15);
             label3.TabIndex = 8;
@@ -134,13 +148,14 @@ namespace TikTok_Downloader
             AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
             BackColor = Color.FromArgb(32, 32, 32);
-            ClientSize = new Size(246, 217);
-            Controls.Add(label2);
-            Controls.Add(label3);
-            Controls.Add(linkLabel);
+            ClientSize = new Size(269, 251);
             Controls.Add(titleLabel);
             Controls.Add(versionLabel);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(licenseInfo);
             Controls.Add(descriptionLabel);
+            Controls.Add(linkLabel);
             Controls.Add(okButton);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
